419d6155b22a8d54d03dd717bbdb1a5aa4015397
Nataliya Lisjo <natlisjo@gmail.com>
2020-09-01 20:12:43 +0200

    C2

--- C2_ReadLine/Program.cs
+++ C2_ReadLine/Program.cs
@@ -6,32 +6,48 @@ namespace C2_ReadLine
     {
         static void Main(string[] args)
         {
-            string name = String.Empty ;
+            string name = string.Empty ;             //variable decklarerd,( tar minne i ram, decklarera när vi behöver)
+            int age ;
 
-            Console.Write("Welcome ! what`s your name?")
-                name = Console.ReadLine();
-            Console.Write($ "{name}oh cool man ! we are the same age. I am also {age} {name}");
+            Console.Write("Welcome Dude ! What`s your name?  ");   //göra dialog. fråga nån 
+            name = Console.ReadLine();                          // lässer en rad tecken
 
-            int age
+
+            Console.Write( $"{name} :) How old are you?  ");
+            // age = Convert.ToInt32(  Console.ReadLine());    //  måste göra convertering string - int 
+            // age = int.Parse(Console.ReadLine());    // andra sätt--- likadant
 
 
             try
             {
-                age = Convert.ToInt32(Console.ReadLine()); 
+                age = Convert.ToInt32(Console.ReadLine()); // försök hear string- int
             }
 
               catch
             {
-                Console.WriteLine( "Sorry man ! You must ");
-                Console.Write( "How old are you man ?");
+                Console.WriteLine( "Sorry man ! You must---, try again. ");    // catching fel och skriver ut
+                Console.Write( "How old are you man ?  ");
+
+                try
+                {
+                    age = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Are you damb?   "  );
+                }
+
             }
             finally
             {
-               
+                Console.WriteLine($"oh cool  ! we are the same age. I am also { age } yo.{name} ");
             }
-            Console.WriteLine($ "oh cool man ! we are the same age. I am also {age} {name}");
-            Console.WriteLine($"Dude your name contains {name.Lenght} characters!");
-            Console.Write($ "oh cool man ! we are the same age. I am also {age} {name}");
+            
+            Console.WriteLine( $"oh cool  ! we are the same age. I am also {age} yo. ");
+            Console.WriteLine( $"Dude your name contains { name.Length } characters!");
+            Console.Write( $"{name} where do you live?  ");
+            var city = Console.ReadLine();
+            Console.WriteLine($"Nice! My sister live in {city} .  ");
         }
     }
 }
