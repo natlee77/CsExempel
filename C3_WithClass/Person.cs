0f178287d0e708a043b18c3f63bfb8bf73e23cd7
Nataliya Lisjo <natlisjo@gmail.com>
2020-09-01 22:10:08 +0200

    C3

--- C3_WithClass/Person.cs
+++ C3_WithClass/Person.cs
@@ -4,19 +4,19 @@ using System.Text;
 
 namespace C3_WithClass
 {
-    class Person  //ska använd inom project
-            {
-        public string Name { get; set; }
-        public int Age { get; set; }
+    class Person  //ska använd inom project inte public, alt om en person
+    {                                       // first  
+        public string Name { get; set; }   //skapa 3 st properties-object,ska public-acessbara. de altid på samma ställe
+        public int Age { get; set; }     //de är tomt utan konstruktor. det är fördel for Class, instället att använda variable
         public string City { get; set; }
-    }
-    class Teacher : Person
-    {
-        
-        public Teacher
-            {
-            }
 
+
+        public Person()          // standart tomt konstruktor (ctor) i Class
+        {
+
+        }           
+        
     }
+   
 
 }
--- C3_WithClass/Program.cs
+++ C3_WithClass/Program.cs
@@ -6,7 +6,48 @@ namespace C3_WithClass
     {
         static void Main(string[] args)   //metod
         {
-           var person  = new Person();
+           var person  = new Person();    // 2. göra instance utan var class - en tomt object
+                                          //  person.   // after . vi kan se alla avaliable properties-variable
+
+            Console.Write("Welcome Dude ! What`s your person.Name?  ");   //göra dialog. fråga nån 
+            person.Name = Console.ReadLine();                          // lässer en rad tecken
+
+
+            Console.Write($"{person.Name} :) How old are you?  ");
+            // person.Age = Convert.ToInt32(  Console.ReadLine());    //  måste göra convertering string - int 
+            // person.Age = int.Parse(Console.ReadLine());    // andra sätt--- likadant
+
+
+            try
+            {
+                person.Age = Convert.ToInt32(Console.ReadLine()); // försök hear string- int
+            }
+
+            catch
+            {
+                Console.WriteLine("Sorry man ! You must---, try again. ");    // catching fel och skriver ut
+                Console.Write("How old are you man ?  ");
+
+                try
+                {
+                    person.Age = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Are you damb?   ");
+                }
+
+            }
+            finally
+            {
+                Console.WriteLine($"oh cool  ! we are the same person.Age. I am also { person.Age } yo.{person.Name} ");
+            }
+
+            Console.WriteLine($"oh cool  ! we are the same person.Age. I am also {person.Age} yo. ");
+            Console.WriteLine($"Dude your person.Name contains { person.Name.Length } characters!");
+            Console.Write($"{person.Name} where do you live?  ");
+            person.City = Console.ReadLine();
+            Console.WriteLine($"Nice! My sister live in {person.City} .  ");
         }
     }
     
