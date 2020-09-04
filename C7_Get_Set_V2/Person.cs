bc24bbc5ea51dd7c266d350a18a5a010b3519b3c
Nataliya Lisjo <natlisjo@gmail.com>
2020-09-04 15:56:46 +0200

    C7

---
+++ C7_Get_Set_V2/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C7_Get_Set_V2
+{
+    class Person
+    {
+        public string Name { get; private set; }
+        public int Age 
+        {//v1
+           // get { }// vanlig for Java ; 
+           // set { }//de kan använda privat värdet och sätta de i public folder Public Int Age
+        //v2
+          get
+            {
+                int _age = DateTime.Now.Year - DateOfBirth.Year; // _ (intern  private variable)
+                if (DateTime.Now.DayOfYear < DateOfBirth.DayOfYear)
+                _age = _age - 1;
+               
+                return _age;
+
+            }
+            
+        }
+        public string City { get; private set; }
+        public string NameContains { get; set; }
+        public DateTime DateOfBirth { get; private set; }
+
+
+        public void AskForName()  //public metod- for att nå från Programm
+        {
+            Console.Write("Welcome Dude ! What`s your Name?  ");
+            Name = Console.ReadLine();
+
+            Console.Write($"{Name} :) Nice  ");
+        }
+
+        public void AskForDateOfBirth()
+        {
+            Console.Write("When were you born?  ");
+
+            try
+            {
+                DateOfBirth = Convert.ToDateTime(Console.ReadLine()); // försök convertera - date
+            }
+
+            catch
+            {
+                Console.Write("Sorry man ! I mean yyyy-mm-dd, try again. ");    // catching fel och skriver ut
+                Console.Write("When were you born?  ");
+
+                try
+                {
+                    DateOfBirth = Convert.ToDateTime(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Are you idiot?");
+                }
+
+            }
+
+            Console.WriteLine($"Ooo! cool , we are the same age I am also {Age}");
+        }
+       
+    }
+}
---
+++ C7_Get_Set_V2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C7_Get_Set_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+           
+            person.AskForDateOfBirth();     // att köra function från PERSON.CS
+            person.AskForName();
+            Console.WriteLine(person.Name);
+        }
+    }
+}
