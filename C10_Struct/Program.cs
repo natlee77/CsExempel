e67c920393b31b91ef0c51de256502261fdad465
Nataliya Lisjo <natlisjo@gmail.com>
2020-09-06 13:12:38 +0200

    C10

--- C10_Struct/Program.cs
+++ C10_Struct/Program.cs
@@ -2,63 +2,80 @@
 
 namespace C10_Struct
 {
+    class ClassExemple
+    {   // 2stuck attribut 
+        public int Nr1 { get; set; }
+        public int Nr2 { get; set; }
 
-    class Program
-    { // bygga en struck / på samma sett som vanlig class
 
-
-        struct Person
-        { //  skapa  grupp av  variable
-            public string Name { get; set; }   
-            public int Age { get; set; }     
-            public string City { get; set; }
+        // 1 ctor  med samma namn som class har - med function(metod) 
+        public ClassExemple(int nr1, int nr2) // declarera - nr1,nr2
+        {
+            Nr1 = nr1;  //när matta in nr1 -automatisk gå Nr1
+            Nr2 = nr2;
         }
 
-
-        static void Main(string[] args)
+        public void DisplayValues()    // 1 funktion
         {
-            //var person = new Person();   - i class 
-            Person person = new Person();
-
-            Console.Write("Welcome Dude ! What`s your person.Name?  ");   //göra dialog. fråga nån 
-            person.Name = Console.ReadLine();                          // lägger in .Name
-
-
-            Console.Write($"{person.Name} :) How old are you?  ");
-            // person.Age = Convert.ToInt32(  Console.ReadLine());    //  måste göra convertering string - int 
-            // person.Age = int.Parse(Console.ReadLine());    // andra sätt--- likadant
-
-
-            try
-            {
-                person.Age = Convert.ToInt32(Console.ReadLine()); // försök hear string- int
-            }
-
-            catch
-            {
-                Console.WriteLine("Sorry man ! You must---, try again. ");    // catching fel och skriver ut
-                Console.Write("How old are you man ?  ");
-
-                try
-                {
-                    person.Age = Convert.ToInt32(Console.ReadLine());
-                }
-                catch
-                {
-                    Console.WriteLine("Are you damb?   ");
-                }
-
-            }
-            finally
-            {
-                Console.WriteLine($"oh cool  ! we are the same person.Age. I am also { person.Age } yo.{person.Name} ");
-            }
-
-            Console.WriteLine($"oh cool  ! we are the same person.Age. I am also {person.Age} yo. ");
-            Console.WriteLine($"Dude your person.Name contains { person.Name.Length } characters!");
-            Console.Write($"{person.Name} where do you live?  ");
-            person.City = Console.ReadLine();
-            Console.WriteLine($"Nice! My sister live in {person.City} .  ");
+            Console.WriteLine($"Display Values:{Nr1}, {Nr2} ");
         }
     }
+
+    struct StruckExemple   //exect likadant som Class , bara namn
+    {   // 2stuck attribut 
+        public int Nr1 { get; set; }
+        public int Nr2 { get; set; }
+
+
+        // ctor  med samma namn som class har - med function(metod) 
+        public StruckExemple(int nr1, int nr2) // declarera - nr1,nr2
+        {
+            Nr1 = nr1;  //när matta in nr1 -automatisk gå Nr1
+            Nr2 = nr2;
+        }
+
+        public void DisplayValues()
+        {
+            Console.WriteLine($"Display Values:{Nr1}, {Nr2} ");
+        }
+    }
+
+    // nu vi ska göra nånting med de (CLASS ; STRUCK) för att visa skilnad eller om de identiska
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ClassExemple -HEAP(GLOBAL)");// alt som jag sparar i HEAP är globala
+            var classexemple = new ClassExemple(1, 1);
+
+            classexemple.DisplayValues(); //function som skriver ut värderna
+            ModifyValues(classexemple);  //tar och köra" ModifyValues" och skicka in "classexemple"
+
+
+            Console.WriteLine("\n------------------------------------\n ");
+
+
+            Console.WriteLine("StruckExample - STACK(LOCAL)");//alla saker i STACK -lokcala variable
+            var struckexemple = new StruckExemple(1, 1);
+
+            struckexemple.DisplayValues(); //function som skriver ut värderna
+            ModifyValues(struckexemple);
+
+
+            Console.ReadKey();
+        }
+
+
+        // en ny function:  static -därför att "static  void Main"
+        private static void ModifyValues(dynamic d)// dynamisk--oavsett class eller struck jag köra
+        {
+            d.Nr1 += 1;
+            d.Nr2 += 1;
+
+            Console.WriteLine($"Modifying Values: {d.Nr1}, {d.Nr2}");
+        }
+
+    }
 }
+
