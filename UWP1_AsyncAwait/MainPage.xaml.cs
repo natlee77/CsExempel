6f18e3c9eeee9ed17ed85ed2bd9deb6df51c3d3b
Nataliya Lisjo <natlisjo@gmail.com>
2020-09-07 22:36:55 +0200

    UWP1

---
+++ UWP1_AsyncAwait/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP1_AsyncAwait
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage() // samma som Main
+        {
+            this.InitializeComponent(); // aldrig tabort
+        }
+        private void SyncButton_Click(object sender, RoutedEventArgs e)
+        {
+            ExecutionResult.Text = ""; // göra tomt
+            TimeResult.Text = "";    //göra tomt
+            var watch = Stopwatch.StartNew(); // kolla hur long tid nån tar- starta -
+
+            RunExample(10);
+
+
+            watch.Stop();
+            TimeResult.Text = watch.ElapsedMilliseconds.ToString();
+
+        }
+        private void AsyncButton_Click(object sender, RoutedEventArgs e)
+        { 
+
+        }
+    }
+}
