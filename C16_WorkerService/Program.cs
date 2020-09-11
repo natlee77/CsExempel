5e10742f712e03f1282a986a9a994358ba4aae9e
Nataliya Lisjo <natlisjo@gmail.com>
2020-09-12 00:16:50 +0200

    C16

--- C16_WorkerService/Program.cs
+++ C16_WorkerService/Program.cs
@@ -4,18 +4,48 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace C16_WorkerService
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args)  //funk.
         {
-            CreateHostBuilder(args).Build().Run();
+                      // vi anpassa all info til Workers.cs:
+
+                 Log.Logger = new LoggerConfiguration() // iniciera 
+                    .MinimumLevel.Debug()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                    .Enrich.FromLogContext()
+                    .WriteTo.File(@"D:\workerservice\log\LogFile.txt")
+         // jag bytar .Console till .File & skriver sök väg till ()ska skapa katalog d:\workerservice\log 
+                    .CreateLogger();
+
+           try
+           {
+                    Log.Information("Starting workerservice...");
+                    //log punkt info/skilja sig från Workers.cs <= från annat class Serialog
+                    CreateHostBuilder(args).Build().Run();
+                    // skapa en tjänst <Worker>- nedre i f.
+                    return ;
+           }// om nån fel -> gå till catch
+                catch (Exception ex)
+           {
+                    Log.Fatal(ex, "Workerservice terminated unexpectedly. Errow :{ex.Message}");
+                    return ;
+           }
+             finally
+           {
+                    Log.CloseAndFlush();//CloseAndFlush - alla message skriva ut 
+           }                                                     
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+                    Host.CreateDefaultBuilder(args)
+                .UseSerilog()   //<-Add this line//middelwire=separat tjänst som används utav kunna göra saker
+                .UseWindowsService()  // läga till efter installed MicrExtensHostWindServ
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
--- C16_WorkerService/Worker.cs
+++ C16_WorkerService/Worker.cs
@@ -9,25 +9,73 @@ using Microsoft.Extensions.Logging;
 
 namespace C16_WorkerService
 {
-    public class Worker : BackgroundService
+   public class Worker : BackgroundService  // arv
     {
-        private readonly ILogger<Worker> _logger;
-        private HttpClient _client;       // hämta-skicka ingo-- request-respond
+        //variable:
+        private readonly ILogger<Worker> _logger; // loger till console eller filer
+        private readonly string _url = "https://google.com"; //statisk
+
+        private HttpClient _client; // 1. hämta-skicka info-- request-respond// finns variable "client" men inte deklarerad
+        private HttpResponseMessage  _result ;//response, få tillbacka svar
 
         public Worker(ILogger<Worker> logger)
-            //construktor//dependency injection
+        //construktor = har samma namn som Class
+        //()dependency injection ILogger<Worker>--2 st bestå
         {
             _logger = logger;
             
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+            // jag tillagt --starta tjänsten
         {
-            while (!stoppingToken.IsCancellationRequested)
+            _client = new HttpClient();     //deklarera
+            _logger.LogInformation("The service has been started");
+            return base.StartAsync(cancellationToken);
+        }
+
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        // jag tillagt --stoppa tjänsten
+        {
+            _client.Dispose();   // städda upp funk. för RAMminne
+            _logger.LogInformation("The service has been stopped");//slutta leka 
+            return base.StopAsync(cancellationToken);// gå från
+        }
+
+
+      protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        //metod//ovverride--från abstract class
+        //(CancellationToken stoppingToken)-avbryta sig Asynv funk.=stoppa in & interrapp
+        {
+         while (!stoppingToken.IsCancellationRequested)
             {//skapa instance
+
+                //client = new HttpClient();
+                //deklarera "client"// nackdel-RAMminne -skapa new client varje sec =10000000mln
+                // i <dependence injectin>vi skapa "HttpClient client" i 
+                //ctor"public Worker"==det funkar inte oxså
+
+             try//altid när vi utföra nån form att handling vi gör try-catch att fänga fel medelande
+               // resultat likad oavsett vad
+                {
+                    _result = await _client.GetAsync(_url);
+                    if (_result.IsSuccessStatusCode)
+                        _logger.LogInformation($"The website {_url} is up. Status code= {_result.StatusCode}");
+                    else
+                        _logger.LogInformation($"The website {_url} is down. Status code= {_result.StatusCode}");
+
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogInformation($"{ex.Message}");
+                }
+               
                 
-                await _client.GetAsync("https://google.com");
-                await Task.Delay(1000, stoppingToken);
+
+                await Task.Delay(10*1000, stoppingToken); // check  varje 10 sec
+                //ligga=sova=vänta 10 sec, om jag vill vakna , då använda "stoppingToken"
             }
         }
     }
